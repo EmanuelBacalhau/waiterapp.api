727ded1d01380eddcab15f885a2542e16607de95
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-12-08 17:17:51 -0300

    feat: add models

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import express  from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017/waiterapp')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-})
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.error('Error connecting to MongoDB');
+  });
